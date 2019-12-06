e1ea00c346d0dcc08571ae5f6907c65795353c90
Michael Zill <michael.zill@gmail.com>
2019-12-06 17:23:50 +0100

    added installation scripts

---
+++ scripts/installPrerequisits.sh
@@ -0,0 +1,15 @@
+##!/bin/zsh
+
+ROS_DISTRO=melodic
+
+echo "Installing ROS distro $ROS_DISTRO"
+sudo apt-get update
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt install -y vim vim-runtime tmux screen git curl wget ros-$ROS_DISTRO-desktop-full ros-$ROS_DISTRO-ackermann-msgs python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt update
+
+# Get ROS commands
+source /opt/ros/$ROS_DISTRO/setup.zsh
---
+++ scripts/installXAVIERslash.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+# Setup the github/mit-racecar/ ROS structure
+# Usage installXAVIERslash.sh <dirName>
+# dirName defaults to XAVIERslash-ws
+# Fetches RACECAR ROS components, installs dependencies, and then catkin_make the workspace
+
+source /opt/ros/melodic/setup.zsh
+DEFAULTDIR=~/XAVIERslash-ws
+CLDIR="$1"
+if [ ! -z "$CLDIR" ]; then 
+ DEFAULTDIR=~/"$CLDIR"
+fi
+if [ -e "$DEFAULTDIR" ] ; then
+  echo "$DEFAULTDIR already exists; no action taken"
+  echo "Placing XAVIERslash code into $DEFAULTDIR" 
+else 
+  echo "$DEFAULTDIR does not exist; Setting up Workspace."
+  mkdir -p ${DEFAULTDIR}/src
+  cd ${DEFAULTDIR}/src
+  catkin_init_workspace
+  cd ..
+  catkin_make
+fi
+cd "$DEFAULTDIR"
+
+wget -q https://raw.githubusercontent.com/taunusflieger/XAVIERslash-ROS/master/racecar.rosinstall -O "$DEFAULTDIR"/.rosinstall
+
+wstool update
+
+# Install prerequisite packages
+echo "Installing prerequisites"
+source devel/setup.zsh
+# Install the rosdeps -a = all -y = no questions -r = skip errors (for openCV in ZED_Wrapper)
+rosdep install -a -y -r
+# jstest-gtk is added for testing the joystick
+sudo apt-get -y install \
+    jstest-gtk 
+
+echo "Catkin Make"
+# On the Jetson, there's currently an issue with using the dynamic runtime
+# Typically this reports as "cannot find -lopencv_dep_cudart" in the error log
+catkin_make config --cmake-args -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
+catkin_make
