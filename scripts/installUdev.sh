92b1c17e99947c6b8bacccfc24ccf59e9afe1d52
Michael Zill <michael.zill@gmail.com>
2019-12-06 18:01:29 +0100

    added udev rules

---
+++ scripts/installUdev.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+# Copy the RACECAR udev rules to /etc/udev/rules.d
+# The rules alias the Sparkfun 9DoF IMU as IMU and the 
+# Electronic Speed Controller as VESC
+sudo cp ./data/racecar.rules /etc/udev/rules.d
+# Reread the rules; You may need to physically replug
+sudo udevadm control --reload-rules 
+sudo udevadm trigger
+echo 'RACECARX Rules installed'
